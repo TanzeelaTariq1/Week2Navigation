4b99c9a3c7965401f50748f77ecd208797bea23b
ASUS <tanzeelatariq52@gmail.com>
2023-07-04 20:40:53 +0500

    new

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import MenuScreen from "./Screens/MenuScreen";
-import WelcomeScreen from "./Screens/WelcomeScreen";
+//import MenuScreen from "./Screens/MenuScreen";
+import LoginScreen from './Screens/LoginScreen';
+import WelcomeScreen from './Screens/WelcomeScreen';
+import MenuScreen from './Screens/MenuScreen';
+//import WelcomeScreen from "./Screens/WelcomeScreen";
 
 
 const Stack = createNativeStackNavigator();
@@ -11,7 +14,13 @@ const Stack = createNativeStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerStyle:{backgroundColor:'#FBDABB'}}}>
+      <Stack.Navigator initialRouteName="Welcome" screenOptions={{headerStyle:{backgroundColor:'yellow',
+      headerTintcolor:'#fff',
+      headerTitleStyle:{
+        fontweight:'bold',
+        fontsize:45,
+      }
+      }}}>
         <Stack.Screen 
         options={{title:'Home'}}
         name="Welcome" component={WelcomeScreen} />
